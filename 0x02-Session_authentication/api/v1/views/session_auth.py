a281bae44b6bcf558a8ce6f06ea29a1daf767f9e
Joseph Nweke <josephchristo615@gmail.com>
2024-01-18 23:12:52 +0100

    Adds empty list comparison for user

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -19,7 +19,7 @@ def session_login():
     if password == "" or password is None:
         return jsonify({"error": "password missing"}), 400
     user_list = User.search({"email": email})
-    if not user_list or user_list is None:
+    if not user_list or user_list == []:
         return jsonify({"error": "no user found for email"}), 404
     for user in user_list:
         if user.is_valid_password(password):
