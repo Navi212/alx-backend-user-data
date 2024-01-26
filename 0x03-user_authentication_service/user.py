2c64b66a5d65fad3347cdfc6d04765b2e8fdbb3c
Joseph Nweke <josephchristo615@gmail.com>
2024-01-26 04:33:49 +0100

    Updates with pep8

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 """
-The `user` module supplies a Model `User`
-for sqlalchemy database Table.
+Declare a SQLAlchemy model named 'User' corresponding to a
+database table named "users"
 """
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Model"""
+    """
+    Definition of class User
+    """
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
